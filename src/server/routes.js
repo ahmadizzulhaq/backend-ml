4b1b35fca98ab04b1abc9562a2e91ad9576b5ed1
ahmadizzulhaq <167025853+ahmadizzulhaq@users.noreply.github.com>
2024-06-21 21:03:38 +0700

    Add files via upload

---
+++ src/server/routes.js
@@ -0,0 +1,28 @@
+const {
+    postPredictHandler,
+    getPredictHistoriesHandler,
+  } = require("../server/handler");
+  
+  
+  const routes = [
+    {
+      path: "/predict",
+      method: "POST",
+      handler: postPredictHandler,
+      options: {
+        payload: {
+          allow: "multipart/form-data",
+          multipart: true,
+          maxBytes: 1000 * 1000,
+        },
+      },
+    },
+    {
+      path: "/predict/histories",
+      method: "GET",
+      handler: getPredictHistoriesHandler,
+    },
+  ];
+  
+  module.exports = routes;
+  
